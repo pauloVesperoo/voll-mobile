3765ca36db6c2620be098a1dfb304f66af5d04c2
Paulo Vespero <paulovcvespero@gmail.com>
2024-09-09 16:10:15 -0300

    Tela de login Criada

---
+++ src/Login.tsx
@@ -0,0 +1,65 @@
+import { VStack, Image, Text, Box, FormControl, Input, Button, Link} from 'native-base'
+import { TouchableOpacity } from 'react-native';
+import Logo from './assets/Logo.png'
+
+export default function Login() {
+  return (
+    <VStack flex={1} alignItems='center' justifyContent='center' p={5}>
+      <Image source={Logo} alt='Logo Voll'/>
+      <Text
+        fontSize="2xl"
+        fontWeight="bold"
+        color="gray.500"
+        textAlign="center"
+        marginTop={5}
+      >
+        Faça login em sua conta
+      </Text>
+      <Box>
+        <FormControl mt={3}>
+          <FormControl.Label>Email</FormControl.Label>
+          <Input 
+            placeholder='Insira seu endereço de email'
+            size='lg'
+            w='100%'
+            borderRadius='lg'
+            backgroundColor='gray.100'
+            shadow={3}
+          />
+        </FormControl>
+        <FormControl mt={3}>
+          <FormControl.Label>Senha</FormControl.Label>
+          <Input 
+            placeholder='Insira sunha senha'
+            size='lg'
+            w='100%'
+            borderRadius='lg'
+            backgroundColor='gray.100'
+            shadow={3}
+          />
+        </FormControl>
+      </Box>
+      <Button 
+        w='100%'
+        bg='blue.800'
+        mt={10}
+        borderRadius='lg'
+      >
+        Entrar
+      </Button>
+      <Link href='https://www.alura.com.br' mt={2}>
+        Esqueceu sua senha?
+      </Link>
+      <Box w='100%' flexDirection='row' justifyContent='center' mt={8}>
+        <Text>Ainda não tem cadastro?</Text>
+        <TouchableOpacity>
+          <Text color='blue.500'>
+            Faça seu cadastro!
+          </Text>
+        </TouchableOpacity>
+      </Box>
+    </VStack>
+  );
+}
+
+
